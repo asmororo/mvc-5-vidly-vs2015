4dcd746028faa9f48608c4819348f4a4aa720923
Anderson Mororo <asmororo@gmail.com>
2018-09-28 15:55:27 -0300

    Solution - Section 3 Exercise 1

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -28,9 +29,21 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
@@ -38,21 +51,12 @@ namespace Vidly.Controllers
         //[Route("customers/details/{id}")]
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith"},
-                new Customer { Id = 2, Name = "Mary Willians"}
-            };
         }
     }
 }
